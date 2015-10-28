0938cff6a4efaccd2d32c5113860c7590d75120f
DanielWebster <gaminglogos@gmail.com>
2015-10-28 17:17:51 -0400

    Initial commit

---
+++ app/src/main/java/com/covisint/covisnacks/Main.java
@@ -0,0 +1,21 @@
+package com.covisint.covisnacks;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+}
